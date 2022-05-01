e3f2cb9ab63637e9f638b2a233b27e957b724784
lyubolp <lyubo.karev@gmail.com>
2022-05-01 23:00:38 +0300

    Added the executor module

---
+++ src/executor.rs
@@ -0,0 +1,24 @@
+pub mod executor {
+    use crate::program_output::program_output::ProgramOutput;
+    use std::process::Command;
+
+    pub fn execute_command(command: &str, args: Vec<&str>) -> Result<ProgramOutput, String> {
+        let output = Command::new(command)
+            .args(args)
+            .output();
+
+        match output {
+            Ok(process) => {
+                match process.status.code() {
+                    Some(exit_code) => Ok(ProgramOutput::new(process.stdout, process.stderr, exit_code)),
+                    None => Err(String::from("Process has been terminated by a signal"))
+                }
+            },
+            Err(err) => {
+                let mut result = String::from("Can't execute the command").to_owned();
+                result.push_str(err.to_string().as_str());
+                Err(result)
+            }
+        }
+    }
+}
